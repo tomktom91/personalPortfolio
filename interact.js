0edc02b4eb03aa0bf2b9b21d52fc9cbc5f8064c5
Tom <tkinnear91@gmail.com>
2022-11-20 15:32:34 +0000

    basic interactivity complete

--- interact.js
+++ interact.js
@@ -8,6 +8,9 @@ const contactsIcon = document.getElementById("contact-icon");
 //arrow Variables
 const arrowLeft = document.getElementById("button-left");
 const arrowRight = document.getElementById("button-right");
+                                                 
+const arrowLeft2 = document.getElementById("button-left2");  //<!----- SECOND BUTTIONS
+const arrowRight2 = document.getElementById("button-right2");
 
 
 headerButton.onclick = function(){          // ------------Activate Header
@@ -68,4 +71,52 @@ arrowRight.onmouseout = function (){
     arrowRight.children[0].style.borderTop = "";
     arrowRight.children[0].style.borderBottom = "";
     arrowRight.children[0].style.borderLeft = ""; 
+}
+
+arrowLeft.onclick = function (){
+    arrowLeft.parentElement.style.visibility = "hidden";
+    arrowLeft.parentElement.nextElementSibling.style.visibility = "visible";
+}
+
+arrowRight.onclick = function (){
+    arrowRight.parentElement.style.visibility = "hidden";
+    arrowRight.parentElement.nextElementSibling.style.visibility = "visible";
+}
+
+
+
+
+
+                                                        //<!----- SECOND BUTTIONS
+arrowLeft2.onmouseover = function (){
+    arrowLeft2.children[0].style.borderTop = "30px solid transparent";
+    arrowLeft2.children[0].style.borderBottom = "30px solid transparent";
+    arrowLeft2.children[0].style.borderRight = "30px solid #ADADB1";
+}
+arrowLeft2.onmouseout = function (){
+    arrowLeft2.children[0].style.borderTop = "";
+    arrowLeft2.children[0].style.borderBottom = "";
+    arrowLeft2.children[0].style.borderRight = ""; 
+}
+
+
+arrowRight2.onmouseover = function (){
+    arrowRight2.children[0].style.borderTop = "30px solid transparent";
+    arrowRight2.children[0].style.borderBottom = "30px solid transparent";
+    arrowRight2.children[0].style.borderLeft = "30px solid #ADADB1";
+}
+arrowRight2.onmouseout = function (){
+    arrowRight2.children[0].style.borderTop = "";
+    arrowRight2.children[0].style.borderBottom = "";
+    arrowRight2.children[0].style.borderLeft = ""; 
+}
+
+arrowLeft2.onclick = function (){
+    arrowLeft2.parentElement.style.visibility = "hidden";
+    arrowLeft2.parentElement.previousElementSibling.style.visibility = "visible";
+}
+
+arrowRight2.onclick = function (){
+    arrowRight2.parentElement.style.visibility = "hidden";
+    arrowRight2.parentElement.previousElementSibling.style.visibility = "visible";
 }
