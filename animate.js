1ddf184d149b11e99892a12ccf3b75361054bc84
Tom <tkinnear91@gmail.com>
2022-11-20 20:46:03 +0000

    started animate.js

---
+++ animate.js
@@ -0,0 +1,26 @@
+
+
+
+const comingLeftPic = [
+  {transform: "translate(100%,0)"},     
+   
+];
+const leftPosition = [
+  {position: "relative",
+   left: "-50%",
+   left: "50%", },  
+];
+
+const leftTiming = {duration: 2000, 
+                    interations: 1,
+}
+
+const aboutSection = document.getElementById("about-me");
+const rightButton = document.getElementById("button-right");
+
+/* *rightButton.addEventListener('click', () => {
+   aboutSection.animate(leftPosition,leftTiming);
+   //aboutSection.animate(comingLeftPic,leftTiming);
+   
+   aboutSection.style.opacity = "100%";
+}); */
